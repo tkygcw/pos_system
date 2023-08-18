8296c36817d16a19e256cd48a2f5c47c47d4a8e4
leowyongfoo <jacksonleow6@gmail.com>
2023-08-18 10:59:24 +0800

    18/8/2023-YF project update(1.0.19) 1.change to production domain

--- lib/database/domain.dart
+++ lib/database/domain.dart
@@ -4,12 +4,12 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 
 class Domain {
-  static var domain = 'https://pos.lkmng.com/';
-  static var backend_domain = 'https://pos.lkmng.com/';
-  static var qr_domain = 'https://pos-qr.lkmng.com/';
-  // static var domain = 'https://pos.optimy.com.my/';
-  // static var backend_domain = 'https://api.optimy.com.my/';
-  // static var qr_domain = 'https://qr.optimy.com.my/';
+  // static var domain = 'https://pos.lkmng.com/';
+  // static var backend_domain = 'https://pos.lkmng.com/';
+  // static var qr_domain = 'https://pos-qr.lkmng.com/';
+  static var domain = 'https://pos.optimy.com.my/';
+  static var backend_domain = 'https://api.optimy.com.my/';
+  static var qr_domain = 'https://qr.optimy.com.my/';
   static Uri login = Uri.parse(domain + 'mobile-api/login/index.php');
   static Uri branch = Uri.parse(domain + 'mobile-api/branch/index.php');
   static Uri device = Uri.parse(domain + 'mobile-api/device/index.php');
